91e935b21e834ce3cdf08b9eff1eff0fa7aaca03
robbiemallett <robbiemallett@gmail.com>
2020-09-28 12:08:22 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyniviz',
+    version='1.0.0',
+    packages=['pyniviz'],
+    url='',
+    license='',
+    author='Robbie Mallett',
+    author_email='robbie.mallett.17@ucl.ac.uk',
+    description='Tool for visualising .pro output from SNOWPACK'
+)
