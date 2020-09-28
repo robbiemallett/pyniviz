5df34774e28cd9fe41c63477731450c2bb1b8ed7
robbiemallett <robbiemallett@gmail.com>
2020-09-28 13:14:24 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyniviz',
+    version='1.0.0',
+    packages=['pyniviz'],
+    url='',
+    license='',
+    author='Robbie Mallett',
+    author_email='',
+    description=''
+)
